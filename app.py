a514eae46ebede16d7a3b20e2e2caf5118d4e171
karim23657 <64953489+karim23657@users.noreply.github.com>
2021-01-07 21:36:29 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,99 @@
+from flask import Flask, redirect, render_template, request, url_for
 import os
-
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+import requests
+import json
+import sys
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
+app.config["DEBUG"] = True
 
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  email = db.Column(db.String(100))
+Info = []
+@app.route('/a')
+def hello_world():
+    return 'Hello from Flask! kjhjj bjhkh'
 
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
+@app.route('/b')
+def hello_world1():
+    return render_template("html.html")
+
+@app.route('/wibble')
+def wibble():
+    return 'This is my pointless new page'
 
 
-@app.route('/', methods=['GET'])
+@app.route("/")
 def index():
-  return render_template('index.html', users=User.query.all())
+    return render_template("html.html")
+
+@app.route("/test", methods=["GET", "POST"])
+def form():
+    return render_template('form.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def hello():
+    to=request.form['to']
+    say=request.form['say']
+    if to != "":
+        return render_template('greet.html', say=os.getcwd(), to=request.form['to'])
 
 
-@app.route('/user', methods=['POST'])
-def user():
-  u = User(request.form['name'], request.form['email'])
-  db.session.add(u)
-  db.session.commit()
-  return redirect(url_for('index'))
 
-if __name__ == '__main__':
-  db.create_all()
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+@app.route('/test1', methods=['GET', 'POST'])
+def hellodc():
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload File To sourush</h1>
+    <form action="http://frjhgu.pythonanywhere.com/upload" method=post enctype=multipart/form-data>
+      <p><input name="link" id="link" value="link"><input name="name" id="name" value="name">
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+#@app.route('/upload',methods = ['GET','POST'])
+#def upload_file():
+#    if request.method =='POST':
+#        file = request.files['file']
+#        file.save(file.filename)
+#    return 'okkkkkkkk'
+
+
+@app.route('/upload',methods = ['GET','POST'])
+def upload_file():
+    if request.method =='POST':
+        namev =request.form['name']
+        url=request.form['link']
+
+        
+        r = requests.get(url)
+        if namev:
+            #f = open(ppath, 'wb')
+            #f.write(r.content)
+            sample_file = r.content
+            upload_file = {"file": sample_file}
+            urll= 'https://bot.sapp.ir/wUhqub6FyG0ZiPVLkYX33GCI2zz292xb6tyu7-zxB9th8Lw3wCzLyNA9fO0NFvxNtfoYZXGMlU0879Vus1tgMXd0oLomLa3MUdosd3V-ouRv5AwVuXkJ7GrJPumxq5S_u5VeUzlVic5eDrJ0/uploadFile'
+            r1 = requests.post(urll, files = upload_file)
+            x =  r1.text
+            y = json.loads(x)
+            furl=y["fileUrl"]
+            urlsu = "https://bot.sapp.ir/JzdoVqPnRb7rn032wT29fZvNOZUCXX3pR4xp9HFWh0bkQ_8Qtw8j_L9zKZtxEBB3-HiJJjD6W423CyzbhvfHYtLdUpBgN0AXjdlACFTVhmkGNx2aj3VR7-3NghnkQLV3jzolToUeLWalQSLv/sendMessage"
+            data = {
+                "to": "Ke39LIXbMYNlfSwyBk6vK6Rd8kljthh9ef34khcZiYmbWxX3vrbyDWw95xY",
+                "type": "FILE",
+                "body": "tis is file",
+                "time": "1538399819589",
+                "fileName": namev,
+                "fileType": "FILE_TYPE_OTHER",
+                "fileSize": sys.getsizeof(sample_file),
+                "fileUrl": furl
+                }
+            headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+            r3 = requests.post(urlsu, data=json.dumps(data), headers=headers)
+            #filename = 'yuyu'
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return render_template('greet.html', say=r3.text, to=sys.getsizeof(sample_file))
+    return 'okkkkkkkk'
+
